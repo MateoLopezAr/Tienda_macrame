87efc8bbe9fc57c2f471bf57dec84306abe7f41f
Mateo López <163686559+MateoLopezAr@users.noreply.github.com>
2024-11-22 16:42:16 -0500

    Productos y estilos página

---
+++ productos.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeElements = document.querySelectorAll('.fade-in');
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('appear');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.3 });
+
+    fadeElements.forEach(element => {
+        observer.observe(element);
+    })
+})
